f0e2976331e0b0734633bf3fefa7db3b87fa0089
Kode <chumex412@gmail.com>
2026-06-28 17:18:25 +0100

    fix: reverts program id

--- anchor/programs/zol_chess_program/src/lib.rs
+++ anchor/programs/zol_chess_program/src/lib.rs
@@ -13,7 +13,7 @@ pub use errors::*;
 pub use instructions::*;
 pub use state::*;
 
-declare_id!("DYTciCFmtAyXhizahzuQeabHgKbSBq6EcbETSR5kStGG");
+declare_id!("8rma9vAqsVAz6pAWFCqnaGTktkzpVk1TWFQsgH3VEh6Y");
 
 #[program]
 pub mod zol_chess_program {
